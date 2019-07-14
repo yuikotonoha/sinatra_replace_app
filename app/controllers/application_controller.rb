83700f3cf44a8f78452f38d9708d931cd788aa81
yuikotonoha <harutinkudo042@gmail.com>
2019-07-14 12:11:00 +0900

    ニックネーム入力、編集機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  # 以下を追記
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # ここから
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :nickname])
+  end
+  # ここまで追記
 end
